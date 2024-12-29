340b292e3c953f7d0d11deb48a1c04c7b3879d47
Alejandro Cardona <alejo.98k@gmail.com>
2024-12-28 21:23:08 -0500

    Remove: import no usado

--- src/main/java/dev/alecar/parquesalitremagico/controller/VisitaController.java
+++ src/main/java/dev/alecar/parquesalitremagico/controller/VisitaController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import jakarta.servlet.http.HttpSession;
 
