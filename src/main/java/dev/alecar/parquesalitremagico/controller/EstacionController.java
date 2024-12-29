20a352f7ff810605945523e00a4a3e520d864d18
Alejandro Cardona <alejo.98k@gmail.com>
2024-12-28 19:13:50 -0500

    Add: permisos para el empleado administrativo

--- src/main/java/dev/alecar/parquesalitremagico/controller/EstacionController.java
+++ src/main/java/dev/alecar/parquesalitremagico/controller/EstacionController.java
@@ -1,6 +1,8 @@
 package dev.alecar.parquesalitremagico.controller;
 
 import dev.alecar.parquesalitremagico.model.Atraccion;
+import dev.alecar.parquesalitremagico.model.Cargo;
+import dev.alecar.parquesalitremagico.model.Empleado;
 import dev.alecar.parquesalitremagico.model.Estacion;
 import dev.alecar.parquesalitremagico.service.EstacionService;
 import dev.alecar.parquesalitremagico.service.AtraccionService;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 import java.util.List;
 import java.util.stream.Collectors;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/estaciones")
@@ -29,13 +33,31 @@ public class EstacionController {
     }
 
     @PostMapping("/{id}/habilitar")
-    public String habilitarEstacion(@PathVariable Long id) {
+    public String habilitarEstacion(@PathVariable Long id, HttpSession session, RedirectAttributes redirectAttributes) {
+        // Verificar si hay un empleado seleccionado y si es administrativo
+        Empleado empleadoSeleccionado = (Empleado) session.getAttribute("empleadoSeleccionado");
+        String cargoEmpleado = (String) session.getAttribute("cargoEmpleado");
+        
+        if (empleadoSeleccionado == null || !Cargo.ADMINISTRATIVO.name().equals(cargoEmpleado)) {
+            redirectAttributes.addFlashAttribute("error", "Para realizar esta acción debes ser empleado ADMINISTRATIVO");
+            return "redirect:/estaciones";
+        }
+        
         estacionService.habilitarEstacion(id);
         return "redirect:/estaciones";
     }
 
     @PostMapping("/{id}/deshabilitar")
-    public String deshabilitarEstacion(@PathVariable Long id) {
+    public String deshabilitarEstacion(@PathVariable Long id, HttpSession session, RedirectAttributes redirectAttributes) {
+        // Verificar si hay un empleado seleccionado y si es administrativo
+        Empleado empleadoSeleccionado = (Empleado) session.getAttribute("empleadoSeleccionado");
+        String cargoEmpleado = (String) session.getAttribute("cargoEmpleado");
+        
+        if (empleadoSeleccionado == null || !Cargo.ADMINISTRATIVO.name().equals(cargoEmpleado)) {
+            redirectAttributes.addFlashAttribute("error", "Para realizar esta acción debes ser empleado ADMINISTRATIVO");
+            return "redirect:/estaciones";
+        }
+        
         estacionService.deshabilitarEstacion(id);
         return "redirect:/estaciones";
     }
