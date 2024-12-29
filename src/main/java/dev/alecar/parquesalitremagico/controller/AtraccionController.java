143aee304add4cda6d1407af1c0b75bae0b93ca3
Alejandro Cardona <alejo.98k@gmail.com>
2024-12-28 19:18:19 -0500

    Add: permisos para el empleado de mantenimiento

--- src/main/java/dev/alecar/parquesalitremagico/controller/AtraccionController.java
+++ src/main/java/dev/alecar/parquesalitremagico/controller/AtraccionController.java
@@ -1,11 +1,16 @@
 package dev.alecar.parquesalitremagico.controller;
 
 import dev.alecar.parquesalitremagico.model.Atraccion;
+import dev.alecar.parquesalitremagico.model.Cargo;
+import dev.alecar.parquesalitremagico.model.Empleado;
 import dev.alecar.parquesalitremagico.service.AtraccionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/atracciones")
@@ -51,13 +56,31 @@ public class AtraccionController {
     }
 
     @PostMapping("/{id}/disponible")
-    public String setAtraccionDisponible(@PathVariable Long id) {
+    public String setAtraccionDisponible(@PathVariable Long id, HttpSession session, RedirectAttributes redirectAttributes) {
+        // Verificar si hay un empleado seleccionado y si es de mantenimiento
+        Empleado empleadoSeleccionado = (Empleado) session.getAttribute("empleadoSeleccionado");
+        String cargoEmpleado = (String) session.getAttribute("cargoEmpleado");
+        
+        if (empleadoSeleccionado == null || !Cargo.MANTENIMIENTO.name().equals(cargoEmpleado)) {
+            redirectAttributes.addFlashAttribute("error", "Para realizar esta acción debes ser empleado de MANTENIMIENTO");
+            return "redirect:/atracciones";
+        }
+        
         atraccionService.setAtraccionDisponible(id);
         return "redirect:/atracciones";
     }
 
     @PostMapping("/{id}/no-disponible")
-    public String setAtraccionNoDisponible(@PathVariable Long id) {
+    public String setAtraccionNoDisponible(@PathVariable Long id, HttpSession session, RedirectAttributes redirectAttributes) {
+        // Verificar si hay un empleado seleccionado y si es de mantenimiento
+        Empleado empleadoSeleccionado = (Empleado) session.getAttribute("empleadoSeleccionado");
+        String cargoEmpleado = (String) session.getAttribute("cargoEmpleado");
+        
+        if (empleadoSeleccionado == null || !Cargo.MANTENIMIENTO.name().equals(cargoEmpleado)) {
+            redirectAttributes.addFlashAttribute("error", "Para realizar esta acción debes ser empleado de MANTENIMIENTO");
+            return "redirect:/atracciones";
+        }
+        
         atraccionService.setAtraccionNoDisponible(id);
         return "redirect:/atracciones";
     }
